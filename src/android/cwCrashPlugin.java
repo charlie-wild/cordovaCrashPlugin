8305d17241447c169b0e2cf9b879dbc3ec5dd92c
Charli Wild <charlie.wild@thinkmoney.co.uk>
2021-09-01 10:03:07 +0100

    removed toast code and added scaffold

--- src/android/cwCrashPlugin.java
+++ src/android/cwCrashPlugin.java
@@ -1,40 +1,27 @@
-package com.charliwild.cordova.plugin;
+package com.scaffold.plugin;
 
-// Cordova-required packages
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.PluginResult;
+import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
-public class ToastyPlugin extends CordovaPlugin {
-  private static final String DURATION_LONG = "long";
-  @Override
-  public boolean execute(String action, JSONArray args,
-    final CallbackContext callbackContext) {
-      // Verify that the user sent a 'show' action
-      if (!action.equals("show")) {
-        callbackContext.error("\"" + action + "\" is not a recognized action.");
-        return false;
-      }
-      String message;
-      String duration;
-      try {
-        JSONObject options = args.getJSONObject(0);
-        message = options.getString("message");
-        duration = options.getString("duration");
-      } catch (JSONException e) {
-        callbackContext.error("Error encountered: " + e.getMessage());
-        return false;
-      }
-      // Create the toast
-      Toast toast = Toast.makeText(cordova.getActivity(), message,
-        DURATION_LONG.equals(duration) ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT);
-      // Display toast
-      toast.show();
-      // Send a positive result to the callbackContext
-      PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
-      callbackContext.sendPluginResult(pluginResult);
-      return true;
-  }
+
+public class Scaffold extends CordovaPlugin {
+    
+    private static final String Tag = "ScaffoldPlugin"
+
+    @Override
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+
+        
+        if (action.equals("customaction")) {
+
+            ///PUT YOUR CODE HERE
+    
+            Log.i(TAG, "Plugin called!")
+            return true;
+
+        } else {
+            
+            return false;
+        }
+    }
 }
--- www/cwCrashPlugin.js
+++ www/cwCrashPlugin.js
@@ -1,21 +1,5 @@
-// Empty constructor
-function cwCrashPlugin() { }
-
-// The function that passes work along to native shells
-// Message is a string, duration may be 'long' or 'short'
-cwCrashPlugin.prototype.show = function (message, duration, successCallback, errorCallback) {
-    var options = {};
-    options.message = message;
-    options.duration = duration;
-    cordova.exec(successCallback, errorCallback, 'cwCrashPlugin', 'show', [options]);
-}
-
-// Installation constructor that binds cwCrashPlugin to window
-cwCrashPlugin.install = function () {
-    if (!window.plugins) {
-        window.plugins = {};
+module.exports = {
+    customaction: function (name, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Scaffold", "customaction", [name]);
     }
-    window.plugins.cwCrashPlugin = new cwCrashPlugin();
-    return window.plugins.cwCrashPlugin;
 };
-cordova.addConstructor(cwCrashPlugin.install);
