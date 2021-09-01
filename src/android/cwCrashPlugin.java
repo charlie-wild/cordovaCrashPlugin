bd71c6424379006a5c1b969cd40dadb491a2ee06
Charli Wild <charlie.wild@thinkmoney.co.uk>
2021-09-01 11:16:45 +0100

    different boilerplate

--- src/android/cwCrashPlugin.java
+++ src/android/cwCrashPlugin.java
@@ -1,27 +1,98 @@
-package com.scaffold.plugin;
+// declare the name of the Java package our plugin resides in.
+// This is typically lowercase and in reverse-URL format.
+package com.thinkmoney.cordova.plugins.pluginsample;
 
-import org.apache.cordova.*;
+// Next import external types that we depend on.
+// The four included here are always going to be needed for any Cordova plugin.
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+// The above three lines could have been replaced by "import org.apache.cordova.*;" but
+// it is generally considered to import only what you want (unless this gets unwieldy)
 import org.json.JSONArray;
-import org.json.JSONException;
 
-public class Scaffold extends CordovaPlugin {
+// Now we declare our plugin class. It must match the name of the file, be in Pascal case
+// (aka UpperCamelCase), and extend the CordovaPlugin class.
+public class PluginSample extends CordovaPlugin {
     
-    private static final String Tag = "ScaffoldPlugin"
-
+    // Every implementation of CordovaPlugin has an "execute" method. 
+    // This has the following parameters:
+    // * action: the name of the action passed in from the javascript (eg MY_FIRST_METHOD)
+    // * args: a JSONArray containing the parameter values passed in
+    // * callbackContext: a Cordova object used to send results back to the calling application
+    //   (as Cordova plugin methods are asynchronous by nature).
     @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+        // We should always wrap the code in our execute method in a try/catch to ensure
+        // no unhandled error gets thrown. 
+        try {
+            // Typically we will do different things based on the action passed in, and
+            // make assumptions about the number and type of items in the arg array.
+            // Unless these are trivially simple we should abstract these into separate
+            // methods, into which we pass the callback context (so we can return results)
+            // and our unwrapped parameters.
+            // (In some more complex plugins it may be better to save the callback context in
+            // an instance field rather than passing it through long chains of methods.)
+            switch(action) {
+                case "MY_FIRST_METHOD":
+                    // Here we expect to have 2 argments in our array - a string and an int.
+                    runMyFirstMethod(callbackContext, args.getString(0), args.getInt(1));
 
-        
-        if (action.equals("customaction")) {
+                    // We return true to indicate that the action was successfully started,
+                    // even if this later returns an error result.
+                    return true;
 
-            ///PUT YOUR CODE HERE
-    
-            Log.i(TAG, "Plugin called!")
-            return true;
+                default:
+                    // We should always have a catch-all in case some numpty calls our cordova
+                    // action with the wrong action name.
+                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.INVALID_ACTION));
+                    // Return false as there was something wrong with the input
+                    return false;
+            }
+        }
+        catch(Throwable e) {
+            // Throwable is the superclass of all errors/exceptions in Java so catching it
+            // guarantees no errors will go unhandled. Typically we'd just return the error
+            // message, as we do here, though it's possible to be more sophisticated and
+            // return a JSON object containing more info, stack trace etc.
 
-        } else {
-            
+            // "error" is a helper method on CallbackContext that returns a PluginResult
+            // with status "ERROR" and some value. The value can be a string, int or JSONObject.
+            callbackContext.error(e.toString());
+
+            // Return false as there was something wrong with the input
             return false;
         }
+    }
+
+    // So here's the method that actually handles our action. We passed the callbackContext through
+    // so we can send a result to the calling code (yay for asynchronous).
+    private void runMyFirstMethod(CallbackContext callbackContext, String firstParameter, int secondParameter){
+        try {
+            // This is a sample so we won't do anything sophisticated. Just return a success result
+            // containing a formatted string.
+            String result = String.format("You submitted parameters '%s' and %d", 
+                firstParameter, secondParameter);
+            
+            // "success" is a helper method on the CallbackContext that sends a PluginResult
+            // with status "OK" and some value. The value can be a string, int, byte array,
+            // JSONArray or JSONObject - or indeed nothing at all if all you need is to indicate success.
+            callbackContext.success(result);
+        } catch(IllegalFormatException e) {
+            // "error" is a helper method that returns a PluginResult with status "ERROR" and
+            // some value. The value can be a string, int or JSONObject.
+            callbackContext.error(e.toString());
+            return false;
+        }
+        
+        // Note that Java's error handling syntax rules mean that any expected exception type
+        // that can be thrown by methods we call, must be either handled by this method or
+        // else declared on the method signature.
+        // If we removed the "try/catch" in this method we would need to update the method
+        // signature to: 
+        
+        // private void runMyFirstMethod(CallbackContext callbackContext, String firstParameter, int secondParameter) throws IllegalFormatException {
+
+        // This would be OK as the outer "execute" method catches all Throwables.
     }
 }
--- www/cwCrashPlugin.js
+++ www/cwCrashPlugin.js
@@ -1,5 +1,31 @@
-module.exports = {
-    customaction: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Scaffold", "customaction", [name]);
-    }
+/* 
+This file creates the Javascript wrappers for our native Cordova actions.
+
+First we require in the Cordova Exec function
+which we will use to call our native Cordova actions from Javascript.
+*/
+var exec = require('cordova/exec');
+
+/* 
+Now we can declare our Javascript methods for wrapping the native Cordova actions;
+these are declared as exports which will be made available on the "window.plugins.pluginSample" 
+object (see the "clobbers" section in plugin.xml)
+By convention the Javascript method is named in camelCase and takes two parameters for the
+success and error callbacks, followed by any additional parameters. Note that in Javascript these
+are untyped, but they will be unwrapped into fixed types in the native code so some discipline is
+needed.
+The method simply calls the cordova "exec" method imported above, passing in the following arguments:
+  * success callback (function to be called if action returns success)
+  * error callback (function to be called if action returns failure)
+  * Name of the plugin class - here "PluginSample" - which must be matched by the name of the class
+    in both iOS and Android implementations
+  * Name of the action. Convention is to name this in UPPER_SNAKE_CASE. This will be passed as a
+    parameter into the Android plugin; and will match the name of a method in the iOS plugin class.
+  * An array containing the parameters. This will be available in the native classes as a
+    JSONArray (Android Java) or NSArray (Objective-C)
+*/
+exports.myFirstMethod = function (success, error, myParameter1, myParameter2) {
+    exec(success, error, 'PluginSample', 'MY_FIRST_METHOD', [myParameter1, myParameter2]);
 };
+
+// and same again for methods if required
